692b066673e485d179f7cf3743a762711e7b53a0
Pablo Escamilla Anguiano <pescamil@Pablos-MacBook-Air.local>
2026-05-19 19:20:36 -0600

    postgresql connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.db.database import engine
+from sqlalchemy import text
 
 app = FastAPI()
 
@@ -6,3 +8,19 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "mi_calli running"}
+
+@app.get("/health/db")
+def db_health():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+
+        return {
+            "database": "connected"
+        }
+
+    except Exception as e:
+        return {
+            "database": "disconnected",
+            "error": str(e)
+        }
